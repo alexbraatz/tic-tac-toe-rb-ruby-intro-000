ed76d97d4c20958a1eff9cebc2372b7374316111
Alex Braatz <alexjbraatz@gmail.com>
2020-05-14 22:26:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,8 +81,7 @@ def won?(board)
     if position1 == "X" && position2 = "X" && position3 == "X" ||
         position1 == "O" && position2 = "O" && position3 = "O"
         return win_combo
-    else
-      false
     end
   end
+  return false
 end
